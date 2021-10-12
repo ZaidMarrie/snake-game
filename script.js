51e05ed320f848fd0fa65d629f92a9cdb95275a1
ZaidMarrie <zaidmarrie27@gmail.com>
2021-10-12 19:06:06 +0200

    clear js file to start from scratch without help

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const gameGrid = document.querySelector('.game-grid');
-const startButton = document.querySelector('.start-btn');
-const scoreElement = document.querySelector('.score');
-const speedElement = document.querySelector('.speed');
-let gridSquares = [];
-let snake = [55, 54, 53];
-let direction = 1;
-const squareWidth = 10;
-let score = 0;
-let intervalTime = 1000;
-let speedMultiplier = 0.9;
-let timerId = 0;
+// Happy Coding!
